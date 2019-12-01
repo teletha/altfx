3f110f363311dfa84c99f178fcdd685e6e749041
Teletha <Teletha.T@gmail.com>
2019-12-01 23:18:10 +0900

    Replace Node implementation.

---
+++ src/project/java/Project.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2019 apocryphal-viewtify Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+public class Project extends bee.api.Project {
+
+    {
+        product("com.github.teletha", "customfx", "1.0");
+
+        require("org.openjfx", "javafx-controls");
+        require("org.openjfx", "javafx-media");
+        require("org.openjfx", "javafx-web");
+
+        versionControlSystem("https://github.com/teletha/viewtify-cutomfx");
+    }
+}
