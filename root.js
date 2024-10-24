4d860a635ef4808c7eb5c1f33da19ed6fc58d852
teletha <teletha@users.noreply.github.com>
2024-10-24 01:12:14 +0000

    deploy: 5331a809d05650a48b9bdbb35d4024e01ccdede9

---
+++ root.js
@@ -0,0 +1,23 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"javafx.scene"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Node",
+			"packageName": "javafx.scene",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Node.StyleableProperties",
+			"packageName": "javafx.scene",
+			"type": "Class"
+		}
+	]
+}
